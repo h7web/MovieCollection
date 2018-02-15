405b76515cac7d32b9a9b96912e298997e0db24e
mikesweb <mikesweb@illinois.edu>
2018-02-15 12:52:57 -0600

    add/remove actors from movies.

--- herman/Controllers/HomeController.cs
+++ herman/Controllers/HomeController.cs
@@ -124,6 +124,23 @@ namespace herman.Controllers
 
             return View(getactor);
         }
+        public ActionResult ViewDirector(int id)
+        {
+            var getdir = (from d in db.directors
+                            where d.dir_id == id
+                            select d).SingleOrDefault();
+
+            getdir.mymovies = (from v in db.Videos where v.Director == id
+                                 orderby v.Release_Date descending
+                                 select new ActorVideo
+                                 {
+                                     video_id = v.video_id,
+                                     Video_Name = v.Video_Name,
+                                     Release_Date = v.Release_Date
+                                 }).ToList();
+
+            return View(getdir);
+        }
 
         public List<SelectListItem> GetCategories(string selected = null)
         {
@@ -171,7 +188,6 @@ namespace herman.Controllers
 
             return PartialView(ret);
         }
-
 
         public int FindDir(string val)
         {
@@ -292,6 +308,108 @@ namespace herman.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        public List<SelectListItem> GetActors(string selected = null)
+        {
+            List<SelectListItem> actors = new List<SelectListItem>();
+
+            actors.Add(new SelectListItem { Value = "", Text = "* Select Actor" });
+
+            var actrs = (from a in db.actors orderby a.actor_last_name, a.actor_first_name select a).ToList();
+
+            foreach (var a in actrs)
+            {
+                actors.Add(new SelectListItem { Value = a.actor_id.ToString(), Text = a.actor_fullname, Selected = (selected == a.actor_id.ToString()) ? true : false });
+            }
+
+            return actors;
+        }
+
+        public List<SelectListItem> GetChars(string selected = null)
+        {
+            List<SelectListItem> chars = new List<SelectListItem>();
+
+            chars.Add(new SelectListItem { Value = "", Text = "* Select Character" });
+
+            var chrs = (from c in db.characters orderby c.char_last_name, c.char_first_name select c).ToList();
+
+            foreach (var c in chrs)
+            {
+                chars.Add(new SelectListItem { Value = c.char_id.ToString(), Text = c.char_fullname, Selected = (selected == c.char_id.ToString()) ? true : false });
+            }
+
+            return chars;
+        }
+
+        public ActionResult AddActors(int id)
+        {
+            var getvideo = (from v in db.Videos
+                            where v.video_id == id
+                            join d in db.directors on v.Director equals d.dir_id
+                            join c in db.categories on v.Category equals c.category_id
+                            select new VideoViewModel
+                            {
+                                video_id = v.video_id,
+                                Video_Name = v.Video_Name,
+                                Release_Date = v.Release_Date
+                            }).SingleOrDefault();
+
+            return View(getvideo);
+        }
+
+        [HttpPost]
+        public ActionResult AddActors(int id, actor a, character c)
+        {
+            actor2movie a2m = new actor2movie();
+
+            if (a.actor_id == 0)
+            {
+                db.actors.Add(a);
+                db.SaveChanges();
+            }
+
+            if (c.char_id == 0)
+            {
+                db.characters.Add(c);
+                db.SaveChanges();
+            }
+
+            a2m.video_id = id;
+            a2m.actor_id = a.actor_id;
+            a2m.char_id = c.char_id;
+
+            db.actor2movie.Add(a2m);
+            db.SaveChanges();
+
+            return RedirectToAction("VideoDetails", new { @id = id });
+        }
+
+        public ActionResult RemoveActor(int video_id, int actor_id, int char_id)
+        {
+            actor2movie a2m = (from a2 in db.actor2movie where a2.video_id == video_id && a2.actor_id == actor_id && a2.char_id == char_id select a2).SingleOrDefault();
+
+            db.actor2movie.Remove(a2m);
+
+            var ainm = from a2 in db.actor2movie where a2.actor_id == a2m.actor_id select a2;
+
+            if (ainm.Count() < 2)
+            {
+                var actor = (from a in db.actors where a.actor_id == a2m.actor_id select a).FirstOrDefault();
+                db.actors.Remove(actor);
+            }
+
+            var cinm = from a2 in db.actor2movie where a2.char_id == a2m.char_id select a2;
+
+            if (cinm.Count() < 2)
+            {
+                var chr = (from c in db.characters where c.char_id == a2m.char_id select c).FirstOrDefault();
+                db.characters.Remove(chr);
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("VideoDetails", new { @id = a2m.video_id });
         }
     }
 }
---
+++ herman/Models/Partialdirector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace herman.Models
+{
+    public partial class director
+    {
+        public List<ActorVideo> mymovies { get; set; }
+    }
+}
